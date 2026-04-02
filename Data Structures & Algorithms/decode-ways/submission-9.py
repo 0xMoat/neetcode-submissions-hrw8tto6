010aaee5a3ff27418f62f5680957fe61d30867af
0xMoat <moatmintmind@gmail.com>
2026-04-03 07:23:58 +0800

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == "0":
+            return 0
+            
+        prev2, prev1 = 1, 1
+        for i in range(2, len(s)+1):
+            temp = 0
+            if s[i-1] != "0":
+                temp += prev1
+
+            if 10 <= int(s[i-2:i]) <= 26:
+                temp += prev2
+            
+            prev2, prev1 = prev1, temp
+        
+        return prev1
