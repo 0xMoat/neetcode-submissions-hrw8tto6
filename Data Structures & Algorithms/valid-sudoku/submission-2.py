eb013617616cecc3097b5e5f78a075a402502b09
0xMoat <moatmintmind@gmail.com>
2026-05-10 10:23:53 +0800

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_checker = collections.defaultdict(set)
+        col_checker = collections.defaultdict(set)
+        grid_checker = collections.defaultdict(set)
+        COLS = len(board[0])
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if board[i][j] != ".":
+                    n = int(board[i][j])
+                    if n in row_checker[i] or \
+                    n in col_checker[j] or \
+                    n in grid_checker[(i//3, j //3)]:
+                        return False
+
+                    row_checker[i].add(n)
+                    col_checker[j].add(n)
+                    grid_checker[(i//3, j //3)].add(n)
+        return True
