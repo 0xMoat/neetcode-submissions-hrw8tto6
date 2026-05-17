9ab09f5d8efc15d4d4f2e128ae4c17f1396f2aa8
0xMoat <moatmintmind@gmail.com>
2026-05-18 07:40:06 +0800

    Add: pacific-atlantic-water-flow - submission-2

---
+++ Data Structures & Algorithms/pacific-atlantic-water-flow/submission-2.py
@@ -0,0 +1,39 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        ROWS, COLS = len(heights), len(heights[0])
+        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+        res_pac = [[False] * COLS for _ in range(ROWS)]
+        res_atl = [[False] * COLS for _ in range(ROWS)]
+
+        source_pac = []
+        source_atl = []
+        for r in range(ROWS):
+            source_pac.append((r, 0))
+            source_atl.append((r, COLS - 1))
+        for c in range(COLS):
+            source_pac.append((0, c))
+            source_atl.append((ROWS - 1, c))
+
+        def bfs(source, res):
+            q = collections.deque(source)
+            while q:
+                r, c = q.popleft()
+                res[r][c] = True
+                for dr, dc in directions:
+                    nr, nc = r + dr, c + dc
+                    if (0 <= nr < ROWS and 0 <= nc < COLS and
+                    res[nr][nc] == False and
+                    heights[nr][nc] >= heights[r][c]):
+                        q.append((nr, nc))
+                        # res[nr][nc] = True
+
+        bfs(source_pac, res_pac)
+        bfs(source_atl, res_atl)
+
+        res = []
+        for r in range(ROWS):
+            for c in range(COLS):
+                if res_pac[r][c] == True and res_atl[r][c] == True:
+                    res.append([r, c])
+
+        return res
